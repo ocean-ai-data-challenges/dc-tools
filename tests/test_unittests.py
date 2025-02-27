0f96da990b560d2fc8d3d6287f976f29260cae8e
Guillermo Cossio <guillermo.cossio@imt-atlantique.fr>
2025-02-27 14:33:36 +0100

    Changed test_unittests.py to pytest format

--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -1,81 +1,91 @@
 import os
-import unittest
 
 import numpy as np
 import xarray as xr
+import pytest
 
 from dctools.dcio.loader import DataLoader
 from dctools.dcio.saver import DataSaver
 from dctools.processing.gridder import DataGridder
 from dctools.utilities.file_utils import empty_folder
 
+def get_sample_dataset():
+    """Create a sample Xarray dataset."""
+    # Create dimensions for the dataset
+    lat = np.linspace(-90, 90, 3)  # 3 latitude points
+    lon = np.linspace(-180, 180, 4)  # 4 longitude points
 
-class TestDCTools(unittest.TestCase):
-    """Unit tests for DCTools library."""
+    # Create some random temperature data
+    temperature = 15 + 8 * np.random.randn(len(lat), len(lon))
 
-    test_dataset = xr.Dataset()
+    # Define the data in an xarray dataset
+    ds = xr.Dataset(
+        {
+            "temperature": (["lat", "lon"], temperature),
+        },
+        coords={
+            "lat": lat,
+            "lon": lon,
+        },
+    )
+    # define dataset attributes
+    ds.attrs.update(
+        {
+            "title": "Sea temperature",
+            "institution": "IMT Atlantique",
+            "source": "Test suite",
+            "description": "Sample temperature data",
+            "units": "Celsius",
+        }
+    )
+    return ds
 
-    def setUp(self):
-        """Tests configuration."""
-        self.test_output_dir = os.path.join("tests", "test_output")
-        os.makedirs(self.test_output_dir, exist_ok=True)
-        self.test_dataset = self.get_sample_dataset()
-        self.test_file_path = os.path.join(self.test_output_dir, "test.nc")
-
-    def tearDown(self):
-        """Clear after tests."""
-        if os.path.exists(self.test_file_path):
-            os.remove(self.test_file_path)
-        if os.path.exists(self.test_output_dir):
-            empty_folder(self.test_output_dir)
-            os.rmdir(self.test_output_dir)
-
-    def get_sample_dataset(self):
-        # Step 1: Create a sample NetCDF dataset
-        # Create dimensions for the dataset
-        lat = np.linspace(-90, 90, 3)  # 3 latitude points
-        lon = np.linspace(-180, 180, 4)  # 4 longitude points
-
-        # Create some random temperature data
-        temperature = 15 + 8 * np.random.randn(len(lat), len(lon))
-
-        # Define the data in an xarray dataset
-        ds = xr.Dataset(
-            {
-                "temperature": (["lat", "lon"], temperature),
-            },
-            coords={
-                "lat": lat,
-                "lon": lon,
-            },
-        )
-        # define dataset attribures
-        self.test_dataset.attrs.update(
-            {
-                "title": "Sea temperature",
-                "institution": "IMT Atlantique",
-                "source": "Sentinel Satellite",
-                "description": "Sample temperature data",
-                "units": "Celsius",
-            }
-        )
-        return ds
-
-    def test_save_load_dataset(self):
-        """Test dataset loading."""
-        DataSaver.save_dataset(self.test_dataset, self.test_file_path)
-        loaded_ds = DataLoader.load_dataset(self.test_file_path)
-        self.assertIsInstance(loaded_ds, xr.Dataset)
-        self.assertIn("temperature", loaded_ds.variables)
-
-    def test_grid_data(self):
-        """Test gridding data."""
-        gridded_ds = DataGridder.interpolate_to_2dgrid(self.test_dataset)
-
-        self.assertTrue("temperature" in gridded_ds.variables)
-        self.assertEqual(gridded_ds.sizes["lon"], 360)
-        self.assertEqual(gridded_ds.sizes["lat"], 180)
+@pytest.fixture(scope='module')
+def setup_data():
+    """Setup test data."""
+    data = get_sample_dataset()
+    yield data
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture(scope='session')
+def setup_filepath():
+    """Setup test paths."""
+    test_output_dir = os.path.join("tests", "test_output")
+    test_file_path = os.path.join(test_output_dir, "test.nc")
+
+    yield test_file_path
+
+    # Teardown
+    if os.path.exists(test_file_path):
+        os.remove(test_file_path)
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_output_dir():
+    """Test path configuration."""
+    test_output_dir = os.path.join("tests", "test_output")
+    os.makedirs(test_output_dir, exist_ok=True)
+    
+    yield test_output_dir
+
+    # Teardown
+    if os.path.exists(test_output_dir):
+        empty_folder(test_output_dir)
+        os.rmdir(test_output_dir)
+
+def test_save_load_dataset(
+    setup_data,
+    setup_filepath,
+    ):
+    """Test dataset loading."""
+    DataSaver.save_dataset(setup_data, setup_filepath)
+    loaded_ds = DataLoader.load_dataset(setup_filepath)
+    assert isinstance(loaded_ds, xr.Dataset)
+    assert "temperature" in loaded_ds.variables
+
+def test_grid_data(setup_data):
+    """Test gridding data."""
+    gridded_ds = DataGridder.interpolate_to_2dgrid(setup_data)
+
+    assert "temperature" in gridded_ds.variables
+    assert gridded_ds.sizes["lon"] == 360
+    assert gridded_ds.sizes["lat"] == 180
